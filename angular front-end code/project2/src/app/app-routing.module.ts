e6deaefa98b0ad35c242de4c71798f7c85e7846d
princeton woods <vpprinceton@aol.com>
2021-09-19 16:40:22 -0400

    Fixed configs

--- angular front-end code/project2/src/app/app-routing.module.ts
+++ angular front-end code/project2/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArmorComponent } from './components/armor/armor.component';
+import { CraftingComponent } from './components/crafting/crafting.component';
+import { ForumComponent } from './components/forum/forum.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
+import { MobsComponent } from './components/mobs/mobs.component';
+import { PotionsComponent } from './components/potions/potions.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'home', component: MainComponent},
+  { path: 'signup', component: SignupComponent},
+  { path: 'recipes', component: CraftingComponent},
+  { path: 'mobs', component: MobsComponent},
+  { path: 'potions', component: PotionsComponent},
+  { path: 'weapons_armor', component: ArmorComponent},
+  { path: 'gallery', component: GalleryComponent},
+  { path: 'forum', component: ForumComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angular front-end code/project2/src/app/app.module.ts
+++ angular front-end code/project2/src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainComponent } from './components/main/main.component';
+import { MobsComponent } from './components/mobs/mobs.component';
+import { PotionsComponent } from './components/potions/potions.component';
+import { CraftingComponent } from './components/crafting/crafting.component';
+import { ArmorComponent } from './components/armor/armor.component';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { ForumComponent } from './components/forum/forum.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    NavbarComponent,
+    MainComponent,
+    MobsComponent,
+    PotionsComponent,
+    CraftingComponent,
+    ArmorComponent,
+    GalleryComponent,
+    ForumComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
